17be56c2cc17963acb99396f03eee7795882727a
Dave Ceddia <dave@daveceddia.com>
2022-04-10 21:40:28 -0400

    Add overlay window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,39 @@
     windows_subsystem = "windows"
 )]
 
+use std::sync::{Arc, Mutex};
+
+use overlay::{add_overlay, WindowsOverlayView};
+use tauri::Manager;
+
+mod overlay;
+
+struct OverlayState(Arc<Mutex<Option<WindowsOverlayView>>>);
+impl OverlayState {
+    pub fn new() -> Self {
+        OverlayState(Arc::new(Mutex::new(None)))
+    }
+    pub fn set_overlay(&self, overlay: WindowsOverlayView) {
+        *self.0.lock().unwrap() = Some(overlay);
+    }
+}
+
 fn main() {
-    tauri::Builder::default()
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let app = tauri::Builder::default()
+        .manage(OverlayState::new())
+        .build(tauri::generate_context!())
+        .expect("failed to build app");
+
+    app.run(move |handle, event| match event {
+        tauri::RunEvent::Ready => {
+            let overlay = add_overlay(handle);
+
+            // Storing the overlay in state so that it doesn't immediately get
+            // dropped, but, the bug happens whether or not the overlay struct
+            // is kept around.
+            let state: tauri::State<OverlayState> = handle.state();
+            state.set_overlay(overlay);
+        }
+        _ => {}
+    })
 }
